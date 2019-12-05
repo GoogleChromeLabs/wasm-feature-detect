1cba443b02d55029b71daeb5e5bd66bd1ce8a8a0
Ingvar Stepanyan <rreverser@google.com>
2019-12-05 13:21:10 +0100

    Fix size-printer
    Looks like Rollup outputs file with a trailing newline, but renderChunk gets passed a string without one, so sizes printed in console and rounded number in README often got out of sync.
    
    This fixes that by adding a newline before gzipping.

--- rollup-plugins/size-printer.js
+++ rollup-plugins/size-printer.js
@@ -3,7 +3,7 @@ import { gzipSync } from "zlib";
 export default function sizePrinter() {
   return {
     renderChunk(rawSource, chunk, outputOptions) {
-      const buffer = Buffer.from(rawSource);
+      const buffer = Buffer.from(rawSource + "\n");
       const gzipped = gzipSync(buffer, { level: 9 });
       console.log("gzipped size:", gzipped.length);
       console.log("raw size:", buffer.length);
