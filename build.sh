5619b8920a4ae3c00279c7ef3030ec603b821ee5
Ingvar Stepanyan <rreverser@google.com>
2020-05-12 16:58:50 +0100

    Remove build.sh
    We use JavaScript version of WABT now, so we no longer need a Unix build script and a separate command.

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir wabt
-curl -L https://github.com/WebAssembly/wabt/releases/download/1.0.12/wabt-1.0.12-linux.tar.gz | tar -xz -C ./wabt
-
-export PATH=$(echo $PWD/wabt/wabt-*)/:$PATH
-
-npm run build:website
