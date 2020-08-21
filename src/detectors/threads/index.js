5877efb03e710b2ccbd9af52894f45663548008a
Surma <surma@surma.dev>
2020-08-21 16:41:01 +0100

    Invert test

--- src/detectors/threads/index.js
+++ src/detectors/threads/index.js
@@ -13,9 +13,10 @@
 
 export default async moduleBytes => {
   try {
-    const MessageChannelConstructor = await import("worker_threads")
-      .then(m => m.MessageChannel)
-      .catch(() => MessageChannel);
+    const MessageChannelConstructor =
+      typeof MessageChannel !== "undefined"
+        ? MessageChannel
+        : await import("worker_threads").then(m => m.MessageChannel);
     // Test for transferability of SABs (needed for Firefox)
     // https://groups.google.com/forum/#!msg/mozilla.dev.platform/IHkBZlHETpA/dwsMNchWEQAJ
     new MessageChannelConstructor().port1.postMessage(new SharedArrayBuffer(1));
