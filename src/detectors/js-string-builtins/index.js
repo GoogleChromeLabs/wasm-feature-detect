8499aaceea51e04fc3c316a02432942184ec92b7
Christopher Serr <christopher.serr@gmail.com>
2024-09-16 22:02:01 +0200

    Add Support for 3 More Proposals
    - Adds Support for `Exception Handling with exnref`
    - Adds Support for `JS String Builtins Proposal for WebAssembly`
    - Adds Support for `Typed function references`
    - Renames old exception handling to `Legacy Exception Handling`

---
+++ src/detectors/js-string-builtins/index.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2023 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+;; Name: JS String Builtins Proposal for WebAssembly
+;; Proposal: https://github.com/WebAssembly/js-string-builtins
+;; Features:
+*/
+
+export default async () => {
+	try {
+		await WebAssembly.instantiate(
+			Uint8Array.from(
+				atob("AGFzbQEAAAABBgFgAW8BfwIXAQ53YXNtOmpzLXN0cmluZwR0ZXN0AAA="),
+				(m) => m.codePointAt(0),
+			),
+			{},
+			{ builtins: ["js-string"] },
+		);
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
